a609063e0cc46deadda82fc90479b2fd9f57cf77
caolei <caolei@megvii.com>
2023-03-07 11:42:38 +0800

    change v2

--- aws/external-modules/launch.sh
+++ aws/external-modules/launch.sh
@@ -2,4 +2,4 @@
 sudo yum update -y && sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
-sudo docker run -p 3000:3000 ibmcom/guestbook:v1
+sudo docker run -p 3000:3000 ibmcom/guestbook:v2
